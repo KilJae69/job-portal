55d167b525a9830b632842a5e1a3bf1131d15831
KilJae69 <adi.toromanovic@outlook.com>
2025-04-04 13:46:41 +0200

    .

--- app/[locale]/jobs/page.tsx
+++ app/[locale]/jobs/page.tsx
@@ -3,6 +3,7 @@ import JobSearchForm from "@/components/forms/JobSearchForm";
 
 import { Container } from "@/components/shared/Container";
 import PageIntro from "@/components/shared/PageIntro";
+import Image from "next/image";
 
 export default async function JobsPage({
   searchParams,
@@ -18,6 +19,7 @@ export default async function JobsPage({
       <PageIntro title="Jobs" />
       <Container className="my-12 lg:my-24">
         <JobSearchForm keyword={keyword} location={location} />
+        <Image className="transition-all duration-300 filter hover:invert" src="/logo-black.svg" alt="logo" width={200} height={200}/>
       </Container>
     </>
   );
